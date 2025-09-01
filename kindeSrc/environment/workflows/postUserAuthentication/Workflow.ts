2fcc78a7f0f30b856a7dd14a02b4d6e63b2270b9
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 16:54:39 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -1,25 +1,25 @@
-import { accessTokenCustomClaims, onUserTokenGeneratedEvent } from "@kinde/infrastructure";
-
-export const workflowSettings = {
-  id: "addExternalOrgId",
-  trigger: "m2m:token_generation",
-  bindings: { "kinde.accessToken": {} },
-};
-
-export default async function (event: onUserTokenGeneratedEvent) {
-  const accessToken = accessTokenCustomClaims<{ foo_bar: string }>();
-  accessToken.foo_bar = "your_value_here";
-}
-
-
-// import type { onM2mTokenGeneratedEvent } from "@kinde/infrastructure";
+// import { accessTokenCustomClaims, onUserTokenGeneratedEvent } from "@kinde/infrastructure";
 
 // export const workflowSettings = {
 //   id: "addExternalOrgId",
 //   trigger: "m2m:token_generation",
-//   bindings: { "kinde.m2mToken": {} }
+//   bindings: { "kinde.accessToken": {} },
 // };
 
-// export default async function (event: onM2mTokenGeneratedEvent) {
-//   kinde.m2mToken.setCustomClaim("external_org_id", "acme-42");
+// export default async function (event: onUserTokenGeneratedEvent) {
+//   const accessToken = accessTokenCustomClaims<{ foo_bar: string }>();
+//   accessToken.foo_bar = "your_value_here";
 // }
+
+
+import type { onM2mTokenGeneratedEvent } from "@kinde/infrastructure";
+
+export const workflowSettings = {
+  id: "addExternalOrgId",
+  trigger: "m2m:token_generation",
+  bindings: { "kinde.m2mToken": {} }
+};
+
+export default async function (event: onM2mTokenGeneratedEvent) {
+  kinde.m2mToken.setCustomClaim("external_org_id", "acme-42");
+}
