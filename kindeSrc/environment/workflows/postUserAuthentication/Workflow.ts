0039bca7b871c2121f4819d54599523f3eb87d32
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 15:16:10 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -13,8 +13,6 @@ export const workflowSettings: WorkflowSettings = {
 // The workflow code to be executed when the event is triggered
 export default async function Workflow(event: onPostAuthenticationEvent) {
   console.log("Hello world");
+  kinde.m2mToken.setCustomClaim("external_org_idz", "acme-42")
 }
 
-export default async function (event) {
-    kinde.m2mToken.setCustomClaim("external_org_idz", "acme-42");
-  }
