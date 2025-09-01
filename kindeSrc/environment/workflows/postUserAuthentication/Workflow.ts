721b371d4952e6cac1dc7d29623d31f439df62a9
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 15:41:39 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -1,17 +1,17 @@
 import {
-  onM2MTokenGeneratedEvent,
+  onPostAuthenticationEvent,
   WorkflowSettings,
   WorkflowTrigger,
 } from "@kinde/infrastructure";
 
 // The setting for this workflow
 export const workflowSettings: WorkflowSettings = {
-  bindings: { "kinde.m2mToken": {} },
-  id: "onM2MTokenGeneration",
-  trigger: WorkflowTrigger.M2MTokenGeneration,
+  bindings: {},
+  id: "onPostAuthenticationEvent",
+  trigger: WorkflowTrigger.PostAuthentication,
 };
 
 // The workflow code to be executed when the event is triggered
-export default async function Workflow(event: onM2MTokenGeneratedEvent) {
+export default async function Workflow(event: onPostAuthenticationEvent) {
   kinde.m2mToken.setCustomClaim("external_org_id", "acme-42");
 }
