224e0636873421dc8238a8b7b3196fb772f57598
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 17:58:27 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -6,7 +6,7 @@ import {
 } from "@kinde/infrastructure";
 
 export const workflowSettings: WorkflowSettings = {
-  id: "addFoo",
+  id: "addFooBarToToken",
   bindings: { "kinde.accessToken": {} },
   trigger: WorkflowTrigger.UserTokenGeneration,
 };
