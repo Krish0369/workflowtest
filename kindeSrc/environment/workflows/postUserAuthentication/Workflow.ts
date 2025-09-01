3522cec1ac1b456460d7d374428022b9e0c249e2
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 16:49:34 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -1,11 +1,10 @@
 import { accessTokenCustomClaims, onUserTokenGeneratedEvent } from "@kinde/infrastructure";
 
-// Optional: workflow settings configuration
 export const workflowSettings = {
   bindings: { "kinde.accessToken": {} },
 };
 
 export default async function (event: onUserTokenGeneratedEvent) {
   const accessToken = accessTokenCustomClaims<{ foo_bar: string }>();
-  accessToken.foo_bar = "your_value_here"; // or set dynamically, e.g., event.request.ip
+  accessToken.foo_bar = "your_value_here";
 }
