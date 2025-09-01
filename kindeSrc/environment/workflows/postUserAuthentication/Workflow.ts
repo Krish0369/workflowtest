08b0efef401d89d75c8be27414a20574d47a1cb1
Krish0369 <118180890+Krish0369@users.noreply.github.com>
2025-09-01 16:29:31 +1000

    Update Workflow.ts

--- kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
+++ kindeSrc/environment/workflows/postUserAuthentication/Workflow.ts
@@ -1,5 +1,4 @@
 import type { onM2mTokenGeneratedEvent } from "@kinde/infrastructure";
-import { m2mTokenCustomClaims } from "@kinde/infrastructure";
 
 export const workflowSettings = {
   id: "addExternalOrgId",
@@ -8,6 +7,5 @@ export const workflowSettings = {
 };
 
 export default async function (event: onM2mTokenGeneratedEvent) {
-  const m2mToken = m2mTokenCustomClaims<{ external_org_id: string }>();
-  m2mToken.external_org_id = "acme-42";
-};
+  kinde.m2mToken.setCustomClaim("external_org_id", "acme-42");
+}
